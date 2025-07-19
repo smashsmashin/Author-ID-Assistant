4751d6460b1140e7ee35c07877aa16b0b76ef4c3
smashsmashin <64376264+smashsmashin@users.noreply.github.com>
2025-07-19 19:44:00 +0300

    Refactoring. Activation logic changed (#2)
    * Refactoring. Activation logic changed
    
    * Fixed device lock detection bug
    
    * Added log on timer cancel
    
    * configured release signing

--- app/src/main/java/app/smashsmashin/authoridassistant/AppState.java
+++ app/src/main/java/app/smashsmashin/authoridassistant/AppState.java
@@ -1,6 +1,62 @@
 package app.smashsmashin.authoridassistant;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.content.Intent;
+import android.content.ActivityNotFoundException;
+import android.util.Log;
+import android.content.Context;
+
 public class AppState {
+    private static final String TAG = "AuthorIDAssistant";
+
     public static boolean isKeyFobActionPending = false;
     public static boolean shouldActivate = false;
+    private static Handler handler = new Handler(Looper.getMainLooper());
+    private static int timers = 0;
+
+    public static void cancelAllTimers() {
+        handler.removeCallbacksAndMessages(AppState.class);
+        if(timers > 0) {
+            Log.d(TAG, "Canceled " + timers + " timers.");
+            timers = 0;
+        }
+    }
+
+    private static void startTimer(Runnable r, long millis) {
+        handler.postDelayed(r, AppState.class, millis);
+        Log.d(TAG, "Started " + (millis / 1000) + " seconds timer.");
+        timers++;
+    }
+
+    public static void triggerAuthorIDActivity(Context context) {
+        isKeyFobActionPending = true;
+        proceedWithTheActivity(context);
+
+        // ensure to stop it later
+        if (shouldActivate) {
+            cancelAllTimers();
+
+            // Start a 30-seconds timer
+            startTimer(() -> {
+                timers--;
+                Log.d(TAG, "30-seconds timer finished. Attempting to unccheck button.");
+                isKeyFobActionPending = true;
+                shouldActivate = false;
+                proceedWithTheActivity(context);
+            }, 30000);
+        }
+    }
+
+    private static void proceedWithTheActivity(Context context) {
+        try {
+            Intent intent = new Intent();
+            intent.setClassName("com.dma.author.authorid", "com.dma.author.authorid.view.SplashActivity");
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT
+                    | Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            context.startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            Log.e(TAG, e.getMessage());
+        }
+    }
 }
--- app/src/main/java/app/smashsmashin/authoridassistant/MyAccessibilityService.java
+++ app/src/main/java/app/smashsmashin/authoridassistant/MyAccessibilityService.java
@@ -5,18 +5,12 @@ import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 import java.util.List;
-import android.os.Handler;
-import android.os.Looper;
-import android.content.Context;
-import android.content.Intent;
 
 public class MyAccessibilityService extends AccessibilityService {
 
   private static final String TAG = "AuthorIDAssistant";
   private static final String TOGGLE_BUTTON_ID = "com.dma.author.authorid:id/button";
   private static final String TARGET_CLASS = "com.dma.author.authorid.view.TagActivity";
-
-  private final Handler timerHandler = new Handler(Looper.getMainLooper());
 
   @Override
   public void onAccessibilityEvent(AccessibilityEvent event) {
@@ -31,48 +25,38 @@ public class MyAccessibilityService extends AccessibilityService {
       AppState.isKeyFobActionPending = false;
 
       AccessibilityNodeInfo rootNode = getRootInActiveWindow();
-      if (rootNode != null) {
-        List<AccessibilityNodeInfo> nodes = rootNode.findAccessibilityNodeInfosByViewId(TOGGLE_BUTTON_ID);
-        if (nodes != null && !nodes.isEmpty()) {
-          AccessibilityNodeInfo toggleButton = nodes.get(0);
-          Log.d(TAG, "ToggleButton found. Is checked: " + toggleButton.isChecked());
-
-          if (AppState.shouldActivate && !toggleButton.isChecked()) {
-            Log.d(TAG, "ToggleButton is not checked. Performing click to activate.");
-            toggleButton.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-
-            // Start a 1-minute timer
-            timerHandler.postDelayed(() -> {
-              Log.d(TAG, "1-minute timer finished. Attempting to unccheck button.");
-
-              AppState.isKeyFobActionPending = true;
-              AppState.shouldActivate = false;
-
-              Intent intent = new Intent();
-              intent.setClassName("com.dma.author.authorid", "com.dma.author.authorid.view.SplashActivity");
-              intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-              startActivity(intent);
-            }, 45000);
-            Log.d(TAG, "Started 1-minute timer.");
-          } else if (!AppState.shouldActivate && toggleButton.isChecked()) {
-            Log.d(TAG, "ToggleButton is checked. Performing click to deactivate.");
-            toggleButton.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-          } else {
-            Log.d(TAG, "ToggleButton is already in the desired state. No action needed.");
-          }
-
-          // Important: recycle when done
-          toggleButton.recycle();
-        } else {
-          Log.w(TAG, "ToggleButton with ID " + TOGGLE_BUTTON_ID + " not found.");
-        }
-
-        // Important: recycle when done
-        rootNode.recycle();
-      }
+      findAndClickToggleButton(rootNode);
 
       // Close the Author ID app
       performGlobalAction(GLOBAL_ACTION_BACK);
+    }
+  }
+
+  private void findAndClickToggleButton(AccessibilityNodeInfo rootNode) {
+    if (rootNode != null) {
+      List<AccessibilityNodeInfo> nodes = rootNode.findAccessibilityNodeInfosByViewId(TOGGLE_BUTTON_ID);
+      if (nodes != null && !nodes.isEmpty()) {
+        AccessibilityNodeInfo toggleButton = nodes.get(0);
+        Log.d(TAG, "ToggleButton found. Is checked: " + toggleButton.isChecked());
+
+        if (AppState.shouldActivate && !toggleButton.isChecked()) {
+          Log.d(TAG, "ToggleButton is not checked. Performing click to activate.");
+          toggleButton.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+        } else if (!AppState.shouldActivate && toggleButton.isChecked()) {
+          Log.d(TAG, "ToggleButton is checked. Performing click to deactivate.");
+          toggleButton.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+        } else {
+          Log.d(TAG, "ToggleButton is already in the desired state. No action needed.");
+        }
+
+        // Important: recycle when done
+        toggleButton.recycle();
+      } else {
+        Log.w(TAG, "ToggleButton with ID " + TOGGLE_BUTTON_ID + " not found.");
+      }
+
+      // Important: recycle when done
+      rootNode.recycle();
     }
   }
 
